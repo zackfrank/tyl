1cd4386a6b421ffece246e452edb32d864448524
Zack Frank <frank.zack@gmail.com>
2020-01-20 18:12:58 -0500

    Add archived to cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_193847) do
+ActiveRecord::Schema.define(version: 2020_01_20_230718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_12_23_193847) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "archived", default: false
   end
 
   create_table "projects", force: :cascade do |t|
