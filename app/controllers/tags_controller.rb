ef04c272b50ccc7bbf7b0c600f4e0de1613566d3
Zack Frank <frank.zack@gmail.com>
2020-01-20 02:32:15 -0500

    Add create tag route/controller action

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,13 @@
 class TagsController < ApplicationController
   
   def index
-    tags = Tag.all.order(:name)
-    render json: tags.as_json
+    tags = Tag.all.order(:name).as_json
+    render json: tags
+  end
+
+  def create
+    tag = Tag.find_by(name: params[:name]) ? nil : Tag.create(name: params[:name]).as_json
+
+    render json: tag
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get "/tasks" => "tasks#index"
-  post "/tasks" => "tasks#create"
-  patch "/tasks/:id" => "tasks#update"
-
-  get "/tags" => "tags#index"
-
+  resources :tags
   resources :cards
 end
