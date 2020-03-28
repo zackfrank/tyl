180c27a395e9a121d8e687718ad222ae0f7499ea
Zack Frank <frank.zack@gmail.com>
2020-03-28 15:34:42 -0500

    Add user ref to tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_203240) do
+ActiveRecord::Schema.define(version: 2020_03_28_203310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2020_03_28_203240) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "task_tags", force: :cascade do |t|
@@ -78,4 +80,5 @@ ActiveRecord::Schema.define(version: 2020_03_28_203240) do
   end
 
   add_foreign_key "cards", "users"
+  add_foreign_key "tags", "users"
 end
