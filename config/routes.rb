1862fb08205ed77928c69129edfa505b168285c1
Zack Frank <frank.zack@gmail.com>
2020-04-05 14:40:22 -0500

    Add Devise user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  scope :api, defaults: { format: :json } do
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
+  end
+
   resources :tags
   resources :cards
 end
