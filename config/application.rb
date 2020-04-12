9cfe9c7faac782c9f72fd56c63f882c3c4405137
Zack Frank <frank.zack@gmail.com>
2020-04-12 18:19:55 -0500

    Set up sessions controller to return JWT to valid user
    Add JWT strategy to Warden

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Tyl
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Eager load modules and classes which exist in the lib/ directory.
+    config.eager_load_paths << Rails.root.join('lib').to_s
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,11 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.warden do |manager|
+    manager.strategies.add(:jwt, Devise::Strategies::JWT)
+    manager.default_strategies(:scope => :user).unshift :jwt
+  end
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
