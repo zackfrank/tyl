426576a6f86957b0fe4038bf23afbf68a404585a
Zack Frank <frank.zack@gmail.com>
2020-04-12 22:02:35 -0500

    Clean up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,8 @@
 class SessionsController < Devise::SessionsController
-  # skip_before_action :verify_authenticity_token, only: [:create]
   before_action :configure_sign_in_params, only: [:create]
-  respond_to :json, :html
 
   def create
     user = User.find_by_email(sign_in_params[:email])
-    puts sign_in_params
-    puts user.inspect
     if user && user.valid_password?(sign_in_params[:password])
       @current_user = user
     else
@@ -14,25 +10,10 @@ class SessionsController < Devise::SessionsController
     end
   end
 
-  # def after_sign_in_path_for(_resource)
-  # end
-
-  # def show
-  #   render json: session_as_json
-  # end
-
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   end
-
-
-  def session_as_json
-    {
-      logged_in: user_signed_in?
-    }
-  end
-
 end
